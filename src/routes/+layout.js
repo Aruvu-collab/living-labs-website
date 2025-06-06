cb0acb175713ed3f465d5955f06a1db29580c05b
Adhavan <adhavan.sivaraj16@gmail.com>
2025-06-06 18:14:42 +0530

    fix adaptor-static moves

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,8 @@
 //import yaml file
 import site from '$lib/misc/index.yaml?raw'
 import * as YAML from 'yaml';
-
+export const prerender = true;
+export const ssr = true;
 export const load = () => {
 	return {
 		site: YAML.parse(site)
